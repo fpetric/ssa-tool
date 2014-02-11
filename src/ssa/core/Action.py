2a27debc796970610712ed79f5fb3b7add284102
Sean Allred <allred.sean@gmail.com>
2014-02-10 21:56:45 -0500

    Move docs around

--- src/ssa/core/Action.py
+++ src/ssa/core/Action.py
@@ -3,13 +3,23 @@ from TeXableEntity import TeXableEntity
 class Action(TeXableEntity):
     """A function from G, v -> G'
 
-    >>> action = Action(lambda G, v: v['marked'] = True,
-                        'v.marked \gets True',
-                        'Marks $v$')
-    >>> doc(action)
-    'Mark $v$'
-    >>> repr(action)
-    'v.marked \gets True'
+    Create an action like so:
+    
+        >>> action = Action(lambda G, v: v['marked'] = True,
+                            'v.marked \gets True',
+                            'Marks $v$')
+    
+    You can retrieve the documentation and TeX representation of the
+    object as you would a `TeXableEntity`:
+    
+        >>> action.doc
+        'Mark $v$'
+        >>> action.TeX
+        'v.marked \gets True'
+    
+    You can also *call* `Action` objects, passing a graph and node as
+    arguments.  This functionality is deferred to the member function
+    `Action.action`.
     """
     def __init__(self, action = lambda graph, node: graph,
                        as_TeX = None,
