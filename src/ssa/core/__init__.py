12ffffa60a37d4f92766807a944350620b88e066
Sean Allred <allred.sean@gmail.com>
2014-02-25 22:25:04 -0500

    Rename Action -> Move (phase 2...)

--- src/ssa/core/__init__.py
+++ src/ssa/core/__init__.py
@@ -3,6 +3,6 @@ from .. import creation
 from .. import simulation
 
 from TeXableEntity import TeXableEntity
-from Action import Action
+from Move import Move
 from Predicate import Predicate
 from FiniteStateMachine import FiniteStateMachine
