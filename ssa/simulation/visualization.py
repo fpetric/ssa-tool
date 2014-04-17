925860a85a2e12ff57c6223238d21e68f9028cd8
Sean Allred <allred.sean@gmail.com>
2014-04-17 17:19:11 -0400

    Add graph object to painter functions

--- ssa/simulation/visualization.py
+++ ssa/simulation/visualization.py
@@ -26,14 +26,12 @@ class Grapher(tk.Canvas):
         self.layout_algorithm = layout_algorithm
 
     def set_node_painter(self, node_painter):
-        self.paint_node = lambda \
-                          layout, node: node_painter(
-                              self, layout, node)
+        self.paint_node = lambda layout, graph, node: \
+                          node_painter(self, layout, graph, node)
 
     def set_edge_painter(self, edge_painter):
-        self.paint_edge = lambda \
-                          layout, source, sink: edge_painter(
-                              self, layout, source, sink)
+        self.paint_edge = lambda layout, graph, source, sink: \
+                          edge_painter(self, layout, graph, source, sink)
 
     def paint(self, layout=None):
         # clear the screen
@@ -59,21 +57,21 @@ class Grapher(tk.Canvas):
                              for tree in positions}
 
         for edge in self.graph.edges():
-            self.paint_edge(normalized_layout, *edge)
+            self.paint_edge(normalized_layout, self.graph, *edge)
         for node in self.graph.nodes():
-            self.paint_node(normalized_layout, node)
+            self.paint_node(normalized_layout, self.graph, node)
 
     @staticmethod
-    def plain_node_painter(canvas, layout, node):
+    def plain_node_painter(canvas, layout, graph, node):
         canvas.create_text((layout[node][0], layout[node][1]), text=str(node))
 
     @staticmethod
-    def plain_edge_painter(canvas, layout, source, sink):
+    def plain_edge_painter(canvas, layout, graph, source, sink):
         canvas.create_line(layout[source][0],    layout[source][1],
                            layout[sink]  [0],    layout[sink]  [1],
                            width=2.0)
     @staticmethod
-    def circle_node_painter(canvas, layout, node):
+    def circle_node_painter(canvas, layout, graph, node):
         r = 10
         x = layout[node][0]
         y = layout[node][1]
@@ -87,34 +85,37 @@ class Grapher(tk.Canvas):
 import networkx   as nx
 import generators as gen
 
-G = gen.random_graph(5)
+def new_graph():
+    gen.reset_basic_node_counter()
+    grapher.set_graph(gen.sparse_graph(10, marked='bool(.5)'))
+    grapher.paint()
+
+def marked_node_painter(canvas, layout, graph, node):
+        r = 10
+        x = layout[node][0]
+        y = layout[node][1]
+        canvas.create_oval((x-r, y-r, x+r, y+r),
+                           fill='black' if graph.node[node]['marked'] else 'white',
+                           tags='node')
+        canvas.create_text((x, y), text=str(node),
+                           fill='white' if graph.node[node]['marked'] else 'black',
+                           tags='node')
 
 root = tk.Tk()
 
 root.title('Graph Painter 2000')
 root.geometry('640x480+5+5')
 
-grapher = Grapher(root, width=200, height=200)
-print(grapher.winfo_width(), grapher.winfo_height())
+grapher = Grapher(root, width=400, height=400, background='gray')
 grapher.pack()
+grapher.set_layout_algorithm(nx.circular_layout)
+grapher.set_node_painter(marked_node_painter)
 
-grapher.create_text((50, 50), text='hello world')
-grapher.create_line(5, 5, 100, 50)
-
-if isinstance(grapher, Grapher):
-    grapher.set_graph(G)
-    grapher.set_layout_algorithm(nx.spring_layout)
-    grapher.set_node_painter(Grapher.circle_node_painter)
-    grapher.paint()
-
-def new_graph(event):
-    gen.reset_basic_node_counter()
-    grapher.set_graph(gen.random_graph(10))
-    grapher.paint()
+new_graph()
 
 # TODO can totally add event handlers for node clicks since the canvas
 # is just a collection of items
-grapher.bind('<Button-1>', new_graph)
+tk.Button(root, text='New Graph', command=new_graph).pack()
  
 # Local Variables:
 # python-shell-interpreter: "python3"
